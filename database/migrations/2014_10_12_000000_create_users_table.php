5a7e06fdea12abae1e8e64cf6fbd6c77ea1dd0c3
livesub <livesub@naver.com>
2021-11-05 16:59:36 +0900

    211105_06

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('user_tel')->comment('집 전화번호');
             $table->string('user_birth', 10)->comment('생년월일');
             $table->enum('user_gender', ['M', 'W'])->comment('성별');
+            $table->enum('user_promotion_agree', ['N', 'Y'])->comment('프로모션 관련 동의 사항');
             $table->string('user_phone')->comment('휴대 전화번호');
             $table->string('user_imagepath')->nullable()->comment('프로필사진 변경파일이름');
             $table->string('user_ori_imagepath')->nullable()->comment('프로필사진 원본파일이름');
