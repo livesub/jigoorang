a96f5bb2412a044e493b063314e93e3c22b96adf
livesub <livesub@naver.com>
2022-01-10 18:12:45 +0900

    220110_07

--- public/design/js/flieupload.js
+++ public/design/js/flieupload.js
@@ -113,6 +113,8 @@ function changeWriteFile(uploadedUrls) {
         }
     } else if (attatchedFiles.length > 0) {
         for (const file of attatchedFiles) {
+
+alert(file.size);
             makePreviewDiv(filesEl, file);
         }
     }
