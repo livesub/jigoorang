81981af8e4c6e18063106c8d1e84fa8e5e150308
livesub <livesub@naver.com>
2022-01-20 13:42:32 +0900

    220120_07

--- public/design/js/text_more_btn.js
+++ public/design/js/text_more_btn.js
@@ -28,3 +28,32 @@ function more(cnt) {
     }
 }
 
+function shop_more(cnt) {
+    var content = $('#shop_content_'+ cnt);
+    var content_txt = content.text();
+
+    var btn_more = $('#shop_cot_more_'+ cnt);
+
+    if(content_txt.length < 200){
+        btn_more.hide();
+        content.removeClass('notshort');
+    }
+
+    toggle_content();
+
+    function toggle_content(){
+        if(content.hasClass('short')){
+            // 접기 상태
+            btn_more.html('더보기');
+            content.removeClass('short');
+            content.addClass('notshort');
+        }else{
+            // 더보기 상태
+            btn_more.html('접기');
+            content.html(content_txt);
+            content.addClass('short');
+            content.removeClass('notshort');
+        }
+    }
+}
+
