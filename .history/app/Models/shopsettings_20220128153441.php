b8679557079e06d54e05ea021527596bb1d53982
livesub <livesub@naver.com>
2022-02-04 14:49:51 +0900

    220204_03

---
+++ .history/app/Models/shopsettings_20220128153441.php
@@ -0,0 +1,55 @@
+<?php
+#############################################################################
+#
+#		파일이름		:		shopsettings.php
+#		파일설명		:		쇼핑몰 환경 설정 관리 테이블 Model
+#		저작권			:		저작권은 제작자에 있지만 누구나 사용합니다.
+#		제작자			:		김영섭
+#		최초제작일	    :		2021년 09월 20일
+#		최종수정일		:		2021년 09월 20일
+#
+###########################################################################-->
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class shopsettings extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'company_name',
+        'company_saupja_no',
+        'company_owner',
+        'company_tel',
+        'company_fax',
+        'company_tongsin_no',
+        'company_buga_no',
+        'company_zip',
+        'company_addr',
+        'company_info_name',
+        'company_info_email',
+        'company_bank_use',
+        'company_bank_account',
+        'company_use_point',
+        'member_reg_point',
+        'de_send_cost',
+        'de_send_cost_free',
+        'de_ment_change',
+        'de_ment_change2',
+        'shop_img_width',
+        'shop_img_height',
+        'text_point',
+        'photo_point',
+    ];
+
+    protected $hidden = [
+
+    ];
+
+    protected $casts = [
+
+    ];
+}
---
+++ .history/resources/views/shop/main.blade_20220128134451.php
@@ -0,0 +1,68 @@
+@extends('layouts.head')
+
+@section('content')
+
+
+    <!-- 메인 컨테이너 시작 -->
+    <div class="sub-container">
+
+        <!-- 위치 시작 -->
+        <div class="location">
+            <ul>
+                <li><a href="/">홈</a></li>
+                <li><a href="{{ route('shop.index') }}">지구를 구하는 쇼핑</a></li>
+            </ul>
+        </div>
+        <!-- 위치 끝 -->
+
+        <!-- 타이틀 시작 -->
+        <div class="title_area">
+            <h2>지구쇼</h2>
+            <div class="text_02 wt-nm">
+                지구를 구하는 쇼핑<br>
+                지구를 구하는 다양한 제품들을 지금 바로 만나보세요
+            </div>
+            <div class="line_14-100"></div>
+        </div>
+        <!-- 타이틀 끝 -->
+
+        <!-- 쇼핑카테고리 시작  -->
+        <div class="eval">
+
+            <div class="board">
+                <!-- 리스트 시작 -->
+                <div class="board_wrap">
+                    <div class="ctg_big">
+                        @foreach($cate_infos as $cate_info)
+                            @php
+                                if($cate_info->sca_img == ""){
+                                    $disp_img = asset("img/no_img.jpg");
+                                }else{
+                                    $disp_img_tmp = explode("@@", $cate_info->sca_img);
+                                    $disp_img_cut = $disp_img_tmp[1];
+                                    $disp_img = "/data/shopcate/".$disp_img_cut;
+                                }
+                            @endphp
+                        <div class="box_menu">
+                            <a href="{{ route('sitem','ca_id='.$cate_info->sca_id) }}">
+                                <img src="{{ $disp_img }}" alt="">
+                                <span>{{ $cate_info->sca_name_kr }}</span>
+                            </a>
+                        </div>
+                        @endforeach
+
+                    </div>
+                </div>
+                <!-- 리스트 끝 -->
+            </div>
+
+        </div>
+        <!-- 쇼핑카테고리 끝  -->
+
+
+
+    </div>
+    <!-- 메인 컨테이너 끝 -->
+
+
+@endsection
