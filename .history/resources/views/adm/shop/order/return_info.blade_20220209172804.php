4d4b5b3cc72796695eaa1f51827f995e6f72f857
livesub <livesub@naver.com>
2022-02-09 17:56:50 +0900

    220209_02

---
+++ .history/resources/views/adm/shop/order/return_info.blade_20220209172804.php
@@ -0,0 +1,5 @@
+<table border=1>
+    <tr>
+        <td>교환 요청</td>
+    </tr>
+</table>
---
+++ .history/resources/views/adm/shop/order/return_info.blade_20220209172826.php
@@ -0,0 +1,11 @@
+<table border=1>
+    <tr>
+        <td>교환 요청</td>
+    </tr>
+</table>
+
+<table border=1>
+    <tr>
+        <td>작성일자</td>
+    </tr>
+</table>
